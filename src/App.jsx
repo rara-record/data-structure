9eec3705a25ee6c8eadbfec6be1c0a795531bf77
BORA <70184893+rara-record@users.noreply.github.com>
2022-12-20 15:44:09 +0900

    feat: render tableoriginal

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import React from 'react';
+import issues from "./issues";
+import TableOriginal from "./TableOriginal";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Tabs>
+      <TabList>
+        <Tab>Original</Tab>
+        <Tab>New Data Structure</Tab>
+        <Tab>Use Derived State</Tab>
+        <Tab>No document.getElementById()</Tab>
+        <Tab>Final</Tab>
+      </TabList>
 
-export default App
+      <TabPanel>
+        <TableOriginal issues={issues} />
+      </TabPanel>
+      <TabPanel>
+
+      </TabPanel>
+      <TabPanel>
+
+      </TabPanel>
+      <TabPanel>
+
+      </TabPanel>
+      <TabPanel>
+
+      </TabPanel>
+    </Tabs>
+  );
+};
+
+export default App;
